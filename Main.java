366c32baaf18fe2b62ffeccea5153d1239f58f36
Priscilla Y <34380354+ScriptKitKat@users.noreply.github.com>
2022-06-11 22:13:02 +0000

    ** Changed to next Int

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ class Main {
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
     System.out.println("How much free time do you have today?");
-    int time = scn.next();
+    int time = scn.nextInt();
   }
 }
