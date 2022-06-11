bda8a99159038185ccaf460002d478b3750ddc81
Priscilla Y <34380354+ScriptKitKat@users.noreply.github.com>
2022-06-11 22:12:40 +0000

    added a new scanner and prompt

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    
+    Scanner scn = new Scanner(System.in);
+    System.out.println("How much free time do you have today?");
+    int time = scn.next();
   }
 }
