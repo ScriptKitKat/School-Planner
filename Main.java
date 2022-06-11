d5b9f28e265f1c3c07af3dc6a928ccadfeb2e314
Priscilla Y <34380354+ScriptKitKat@users.noreply.github.com>
2022-06-11 22:16:00 +0000

    Added comments of ideas

--- Main.java
+++ Main.java
@@ -3,7 +3,21 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-    System.out.println("How much free time do you have today?");
+    System.out.println("How much free time do you have today? ");
     int time = scn.nextInt();
+    
   }
+
+  /*
+    The idea:
+    Asks for amount of free time.
+    Asks for assignments and he amount of time it takes
+    Makes a schedule
+
+    If more time:
+    - Make it look fancy
+    - Add priority to list
+    - Saves it onto a ledger so it can update if they come later (this is only if we have A LOT of time)
+    
+  */
 }
